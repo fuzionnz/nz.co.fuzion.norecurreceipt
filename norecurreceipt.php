24fcca32a7e520fd046993e8e0aabbb9a18109e3
eileen <emcnaughton@wikimedia.org>
2024-09-03 08:23:59 +1200

    Civix upgrade

--- norecurreceipt.php
+++ norecurreceipt.php
@@ -10,15 +10,6 @@ function norecurreceipt_civicrm_config(&$config) {
 }
 
 /**
- * Implementation of hook_civicrm_xmlMenu
- *
- * @param $files array(string)
- */
-function norecurreceipt_civicrm_xmlMenu(&$files) {
-  _norecurreceipt_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implementation of hook_civicrm_install
  */
 function norecurreceipt_civicrm_install() {
@@ -26,47 +17,10 @@ function norecurreceipt_civicrm_install() {
 }
 
 /**
- * Implementation of hook_civicrm_uninstall
- */
-function norecurreceipt_civicrm_uninstall() {
-  return _norecurreceipt_civix_civicrm_uninstall();
-}
-
-/**
  * Implementation of hook_civicrm_enable
  */
 function norecurreceipt_civicrm_enable() {
   return _norecurreceipt_civix_civicrm_enable();
-}
-
-/**
- * Implementation of hook_civicrm_disable
- */
-function norecurreceipt_civicrm_disable() {
-  return _norecurreceipt_civix_civicrm_disable();
-}
-
-/**
- * Implementation of hook_civicrm_upgrade
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- */
-function norecurreceipt_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _norecurreceipt_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implementation of hook_civicrm_managed
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- */
-function norecurreceipt_civicrm_managed(&$entities) {
-  return _norecurreceipt_civix_civicrm_managed($entities);
 }
 
 /**
